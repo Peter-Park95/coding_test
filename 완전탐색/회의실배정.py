0c10be7bd041d7c3ef8f9af9736e1cb9597c1b2c
Peter <baaams.park@gmail.com>
2025-04-23 16:15:09 +0900

    그리디,  회의실 배정

---
+++ 완전탐색/회의실배정.py
@@ -0,0 +1,15 @@
+from collections import deque
+N = int(input())
+meetings = deque()
+count = 0
+current_time = 0
+for i in range(N):
+    start, end = map(int, input().split())
+    meetings.append((start, end))
+meetings = deque(sorted(meetings, key=lambda time: (time[1], time[0])))
+while meetings:
+    start_time, end_time = meetings.popleft()
+    if start_time >= current_time:
+        count += 1
+        current_time = end_time
+print(count)
