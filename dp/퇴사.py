1c488c0a3e084fa7d90df07d8988a6bb38b03223
Peter <baaams.park@gmail.com>
2025-05-04 21:22:43 +0900

    퇴사문제

--- dp/퇴사.py
+++ dp/퇴사.py
@@ -5,4 +5,6 @@ for _ in range(N):
     t, p = map(int, input().split())
     consulting.append((t,p))
 
-# for day in (1, N+1):
+for day in (0, N+1):
+    income[day] = income[day-1] + consulting[day][1]
+    
